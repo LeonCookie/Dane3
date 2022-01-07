5230aab56a331df2bf6aba04d81176e39a585f42
LeonCookie <leongdynia1105@gmail.com>
2022-01-07 22:19:42 +0100

    nie działa :)

--- app/src/main/java/com/example/dane3/MainActivity.java
+++ app/src/main/java/com/example/dane3/MainActivity.java
@@ -15,17 +15,19 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
     String[] UczenList;
-
+    //dane dla fragmetu 1
     InputStream inputStreamCounterUczen;
     BufferedReader bufferedReaderCounterUczen;
-
     InputStream inputStreamLoaderUczen;
     BufferedReader BufferedReaderLoaderUczen;
-    
+    int intCountUczen;
+
+   //====
     BottomNavigationView bottomNavigationView;
     @RequiresApi(api = Build.VERSION_CODES.O)
     @SuppressLint("WrongViewCast")
@@ -60,10 +62,31 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        //========-=========
+        //========-========= dodanie elementow do fragemtu 1
         ListView listView= (ListView) findViewById(R.id.litsview_fragment1);
-        inputStreamCounterUczen = this.getResources().openRawResource(R.raw.daneUczen);
+        inputStreamCounterUczen = this.getResources().openRawResource(R.raw.dane_uczen);
+        bufferedReaderCounterUczen = new BufferedReader(new InputStreamReader(inputStreamCounterUczen));
 
+        inputStreamLoaderUczen = this.getResources().openRawResource(R.raw.dane_uczen);
+        BufferedReaderLoaderUczen = new BufferedReader(new InputStreamReader(inputStreamLoaderUczen));
+
+        try{
+            while (bufferedReaderCounterUczen.readLine()!=null){
+            intCountUczen++;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        UczenList = new String[intCountUczen];
+
+        try{
+            for(int i=0;i<intCountUczen; i++){
+                UczenList[i] = BufferedReaderLoaderUczen.readLine();
+
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
 
 
